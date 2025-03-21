70f5d1aef6170a18ea670e5365689340ff590e8a
akshaya <akshaya.pra12@gmail.com>
2025-03-21 22:42:53 +0100

    some styles added

--- yoga-asanas-project-frontend/src/components/NavBar.jsx
+++ yoga-asanas-project-frontend/src/components/NavBar.jsx
@@ -7,9 +7,9 @@ const NavBar=()=>{
     return(
         <nav className="nav-bar">
             <div className="navbar-container">
-            <h2 className="logo">Yoga App</h2>
+            <h2 className="logo">Welcome to our Inner Peace portal</h2>
             <div className="nav-links">
-            <Link to="/">YogaAsanas</Link>
+            <Link to="/asanas">YogaAsanas</Link>
                 <Link to="/about">About Yoga</Link>
                
             </div>
