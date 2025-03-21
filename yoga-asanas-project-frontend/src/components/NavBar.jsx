5ca808c423a820ce870a98b207caf48d6dc3b00f
akshaya <akshaya.pra12@gmail.com>
2025-03-21 18:11:58 +0100

    some changes made

--- yoga-asanas-project-frontend/src/components/NavBar.jsx
+++ yoga-asanas-project-frontend/src/components/NavBar.jsx
@@ -9,8 +9,9 @@ const NavBar=()=>{
             <div className="navbar-container">
             <h2 className="logo">Yoga App</h2>
             <div className="nav-links">
+            <Link to="/">YogaAsanas</Link>
                 <Link to="/about">About Yoga</Link>
-                <Link to="/asanas">YogaAsanas</Link>
+               
             </div>
             </div>
         </nav>
