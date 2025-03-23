9825fed0a2bde69902da50254f77d7c1b608e391
akshaya <akshaya.pra12@gmail.com>
2025-03-23 19:34:43 +0100

    Instructions and some stylings  are added

--- yoga-asanas-project-frontend/src/components/Asana.jsx
+++ yoga-asanas-project-frontend/src/components/Asana.jsx
@@ -1,12 +1,25 @@
 
 function Asana({asana,onDelete,onEdit}){
+  
     return(
         <div className="asana-card">
             <img src={asana.image} alt={asana.name} className="asana-image" />
             <div className="asana-content">
                 <h3>{asana.name}</h3>
-                <p>Benefits:{asana.benefits}</p>
-                <p>Instructions:{asana.instruction}</p>
+                <p><strong>Benefits</strong>{asana.benefits}</p>
+                <div className="instruction-list">
+                <strong>Instructions:</strong>
+                <ul>
+                 {Array.isArray(asana.instruction)?(
+                    asana.instruction.map((step,index)=>(
+                        <p key={index}>{step}</p>
+                    ))
+                ):(
+                    <p>{asana.instruction}</p>
+                 )}
+                </ul>
+                </div>
+
                 <div className="asana-buttons">
                     <button className="delete-btn" onClick={()=>onDelete(asana.id)}>Delete</button>
                     <button className="eidt-btn" onClick={()=>onEdit(asana)}>Edit</button>
