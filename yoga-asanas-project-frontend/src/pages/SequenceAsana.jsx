8f266aac4bbe90ad5b843229383e53839a994258
akshaya <akshaya.pra12@gmail.com>
2025-03-25 18:06:49 +0100

    some changes in sequence

--- yoga-asanas-project-frontend/src/pages/SequenceAsana.jsx
+++ yoga-asanas-project-frontend/src/pages/SequenceAsana.jsx
@@ -1,46 +1,48 @@
-import { useContext, useState } from "react"; //useContext is used to access global context
-import { AsanaContext } from "../context/AsanaContext";
+import {useContext,useState} from "react";//useContext is used to access global context
+import {AsanaContext} from "../context/AsanaContext";
 import Asana from "../components/Asana";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
-function SequenceAsana() {
-  const { selectedAsanas, removeAsana, selectedYogaSetId } =
-    useContext(AsanaContext); // that shows selected asanas and lets us update and delete
-  const [asanas, setAsanas] = useState(selectedAsanas);
-  const [editingId, setEditingId] = useState();
+function SequenceAsana(){
+    const {selectedAsanas,removeAsana,selectedYogaSetId}=useContext(AsanaContext);// that shows selected asanas and lets us update and delete
+    const [asanas,setAsanas]=useState(selectedAsanas);
+    const [editingId,setEditingId]=useState();
 
-  const navigate = useNavigate();
+    const navigate=useNavigate();
 
-  const handleDelete = (asanaId) => {
-    const updated = asanas.filter((a) => a.id !== asanaId);
-    setAsanas(updated);
-    removeAsana(asanaId); //function to delete
-  };
-  const handleEdit = (asana) => {
-    navigate("/edit-asana", {
-      state: {
-        asana,
-        yogaSetId: selectedYogaSetId, //editing a selected yoga id
-      },
-    });
-  };
+    
 
-  return (
-    <div className="sequence-asanas-page">
-      <h2>Selected Asanas</h2>
-      {asanas.length === 0 ? (
-        <p>No asanas selected yet.</p>
-      ) : (
-        asanas.map((asana) => (
-          <Asana
-            key={asana.id}
-            asana={asana}
-            onDelete={handleDelete}
-            onEdit={handleEdit}
-          />
-        ))
-      )}
-    </div>
-  );
+    const handleDelete=(asanaId)=>{
+        const updated=asanas.filter((a)=>a.id!==asanaId);
+        setAsanas(updated);
+        removeAsana(asanaId);//function to delete using remove which updates context and re-renders the list
+    };
+    const handleEdit=(asana)=>{
+       navigate("/edit-asana",{
+        state:{
+            asana,
+            yogaSetId:selectedYogaSetId//editing a selected yoga id
+        }
+       })
+};
+
+
+    return(
+        <div className="sequence-asanas-page">
+            <h2>Selected Asanas</h2>
+            {asanas.length===0?(
+                <p>No asanas selected yet.</p>
+            ):(
+              asanas.map((asana)=>(//renders them using asana components
+                    <Asana
+                    key={asana.id}
+                    asana={asana}
+                    onDelete={handleDelete}
+                    onEdit={handleEdit} />
+                    
+                ))
+        )}
+        </div>
+    );
 }
 export default SequenceAsana;
